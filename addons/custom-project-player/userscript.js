4279c25026af46a9f81ff3a2ff53878fc450c520
FunctionalMetatable <FunctionalMetatable@users.noreply.github.com>
2021-06-04 18:56:31 +0000

    style: format code

--- addons/custom-project-player/userscript.js
+++ addons/custom-project-player/userscript.js
@@ -1,27 +1,29 @@
-export default async function({ addon, global, console, msg }) {
-  addon.tab.addEventListener('urlChange', (e) => addEmbeds(e.detail.newUrl))
-  
-  addEmbeds(window.location.href)
-  
+export default async function ({ addon, global, console, msg }) {
+  addon.tab.addEventListener("urlChange", (e) => addEmbeds(e.detail.newUrl));
+
+  addEmbeds(window.location.href);
+
   async function addEmbeds(projectURI) {
-    if (!projectURI.includes('editor')) {
-      if (!projectURI.includes('fullscreen')) {
-        let guiPlayer = await addon.tab.waitForElement(".guiPlayer", { markAsSeen: true })
-        
-        let stageWrapper = await addon.tab.waitForElement(".guiPlayer [class*='stage-wrapper_stage-wrapper']", { markAsSeen: true })
-        stageWrapper.style.display = 'none';
-        
-        let iframe = document.createElement('iframe')
-        iframe.src = `https://turbowarp.org/${location.pathname.split("/")[2]}/embed`
-        iframe.width = '480px';
-        iframe.height = '392px';
-        iframe.classList.add('sa-custom-player')
-        iframe.classList.add('turbowarp')
-        iframe.setAttribute('allowfullscreen', true)
-        
-        iframe.style = "border: none;"
+    if (!projectURI.includes("editor")) {
+      if (!projectURI.includes("fullscreen")) {
+        let guiPlayer = await addon.tab.waitForElement(".guiPlayer", { markAsSeen: true });
+
+        let stageWrapper = await addon.tab.waitForElement(".guiPlayer [class*='stage-wrapper_stage-wrapper']", {
+          markAsSeen: true,
+        });
+        stageWrapper.style.display = "none";
+
+        let iframe = document.createElement("iframe");
+        iframe.src = `https://turbowarp.org/${location.pathname.split("/")[2]}/embed`;
+        iframe.width = "480px";
+        iframe.height = "392px";
+        iframe.classList.add("sa-custom-player");
+        iframe.classList.add("turbowarp");
+        iframe.setAttribute("allowfullscreen", true);
+
+        iframe.style = "border: none;";
         guiPlayer.appendChild(iframe);
-        
+
         let alerts = await addon.tab.waitForElement(".project-info-alert");
         alerts.remove();
       }
