44cfa2b3a5be89b001668174225556fe4d6097e1
FunctionalMetatable <FunctionalMetatable@users.noreply.github.com>
2021-07-28 12:14:08 +0000

    Format code

--- addons/convert/userscript.js
+++ addons/convert/userscript.js
@@ -1,72 +1,88 @@
 export default async function ({ addon, global, console, msg }) {
-    let username = await addon.auth.fetchUsername()
-    async function createDropdown() {
-        if (document.getElementById('sa-convert-btn')) return
-        let button = document.createElement('div')
-        button.className = addon.tab.scratchClass('menu-bar_menu-bar-item', 'menu-bar_hoverable')
-        button.id = 'sa-convert-btn'
-        let active = false
-        button.addEventListener('click', e => {
-            if (!e.target.closest('#sa-convert-btn')) return;
+  let username = await addon.auth.fetchUsername();
+  async function createDropdown() {
+    if (document.getElementById("sa-convert-btn")) return;
+    let button = document.createElement("div");
+    button.className = addon.tab.scratchClass("menu-bar_menu-bar-item", "menu-bar_hoverable");
+    button.id = "sa-convert-btn";
+    let active = false;
+    button.addEventListener("click", (e) => {
+      if (!e.target.closest("#sa-convert-btn")) return;
 
-            table.style.display = table.style.display == 'block' ? 'none' : 'block'
-            if (active) {
-                button.className = addon.tab.scratchClass('menu-bar_menu-bar-item', 'menu-bar_hoverable')
-                
-                active = false
-            } else {
-                button.className = addon.tab.scratchClass('menu-bar_menu-bar-item', 'menu-bar_hoverable', 'menu-bar_active')
-                active = true
-            }
-        })
-        document.addEventListener('click', e => {
-            if (e.target.closest('#sa-convert-btn')) return
-            if (active) {
-                button.className = addon.tab.scratchClass('menu-bar_menu-bar-item', 'menu-bar_hoverable')
-                table.style.display = 'none'
-                active = false
-            }
-        })
+      table.style.display = table.style.display == "block" ? "none" : "block";
+      if (active) {
+        button.className = addon.tab.scratchClass("menu-bar_menu-bar-item", "menu-bar_hoverable");
 
-        let buttonInner = document.createElement('span')
-        buttonInner.innerText = msg('button')
-        button.appendChild(buttonInner)
+        active = false;
+      } else {
+        button.className = addon.tab.scratchClass("menu-bar_menu-bar-item", "menu-bar_hoverable", "menu-bar_active");
+        active = true;
+      }
+    });
+    document.addEventListener("click", (e) => {
+      if (e.target.closest("#sa-convert-btn")) return;
+      if (active) {
+        button.className = addon.tab.scratchClass("menu-bar_menu-bar-item", "menu-bar_hoverable");
+        table.style.display = "none";
+        active = false;
+      }
+    });
 
-        let dropdown = document.createElement('div')
-        dropdown.className = addon.tab.scratchClass('menu-bar_menu-bar-menu')
-        
-        let table = document.createElement('ul')
-        table.className = addon.tab.scratchClass('menu_menu', 'menu_right')
-        table.style.display = 'none'
+    let buttonInner = document.createElement("span");
+    buttonInner.innerText = msg("button");
+    button.appendChild(buttonInner);
 
-        function createDropdownItem(name, click) {
-            let item = document.createElement('li')
-            item.className = addon.tab.scratchClass('menu_menu-item', 'menu_hoverable', 'menu_menu-section')
+    let dropdown = document.createElement("div");
+    dropdown.className = addon.tab.scratchClass("menu-bar_menu-bar-menu");
 
-            let itemInner = document.createElement('span')
-            itemInner.innerText = name
-            item.appendChild(itemInner)
+    let table = document.createElement("ul");
+    table.className = addon.tab.scratchClass("menu_menu", "menu_right");
+    table.style.display = "none";
 
-            item.addEventListener('click', () => window.open(click(addon.tab.redux.state.preview.projectInfo.id, username, addon.tab.redux.state.preview.projectInfo.title)))
+    function createDropdownItem(name, click) {
+      let item = document.createElement("li");
+      item.className = addon.tab.scratchClass("menu_menu-item", "menu_hoverable", "menu_menu-section");
 
-            return item
-        }
+      let itemInner = document.createElement("span");
+      itemInner.innerText = name;
+      item.appendChild(itemInner);
 
-        table.appendChild(createDropdownItem('HTMLifier', (projectId, username, title) => `https://sheeptester.github.io/htmlifier/?title=${title}&id=${projectId}`))
-        table.appendChild(createDropdownItem("LeapordJS (JavaScript)", (projectId) => `https://leapordjs.vercel.app/api/${projectId}/codesandbox`))
-        table.appendChild(createDropdownItem("TurboWarp Packager", (projectId) => `https://packager.turbowarp.org/?id=${projectId}`))
+      item.addEventListener("click", () =>
+        window.open(
+          click(addon.tab.redux.state.preview.projectInfo.id, username, addon.tab.redux.state.preview.projectInfo.title)
+        )
+      );
 
-        dropdown.appendChild(table)
-        button.appendChild(dropdown)
-
-        let menu = document.querySelector('[class*=menu-bar_file-group]')
-
-        menu.appendChild(button)
+      return item;
     }
 
-    if (addon.tab.editorMode == 'editor') {
-        createDropdown()
-    }
+    table.appendChild(
+      createDropdownItem(
+        "HTMLifier",
+        (projectId, username, title) => `https://sheeptester.github.io/htmlifier/?title=${title}&id=${projectId}`
+      )
+    );
+    table.appendChild(
+      createDropdownItem(
+        "LeapordJS (JavaScript)",
+        (projectId) => `https://leapordjs.vercel.app/api/${projectId}/codesandbox`
+      )
+    );
+    table.appendChild(
+      createDropdownItem("TurboWarp Packager", (projectId) => `https://packager.turbowarp.org/?id=${projectId}`)
+    );
 
-    addon.tab.addEventListener('urlChange', e => addon.tab.editorMode == 'editor' ? createDropdown() : "")
+    dropdown.appendChild(table);
+    button.appendChild(dropdown);
+
+    let menu = document.querySelector("[class*=menu-bar_file-group]");
+
+    menu.appendChild(button);
+  }
+
+  if (addon.tab.editorMode == "editor") {
+    createDropdown();
+  }
+
+  addon.tab.addEventListener("urlChange", (e) => (addon.tab.editorMode == "editor" ? createDropdown() : ""));
 }
