6c3d09340f5047ef176d1d9317aa28fd7c63e759
FunctionalMetatable <FunctionalMetatable@users.noreply.github.com>
2021-07-29 09:07:43 +0000

    Format code

--- addons/hovercards/userscript.js
+++ addons/hovercards/userscript.js
@@ -1,38 +1,40 @@
 export default async function ({ addon, global, console, msg }) {
-    while (true) {
-        let link = await addon.tab.waitForElement("a[href*='/users/']", { markAsSeen: true })
-        let user = link.href.replace(/(.*)\/users\/((?:[a-z]|[A-Z]|[0-9]|-|_)+)\//g, '$2')
-        
-        const hovercard = document.createElement('div')
-        hovercard.className = 'sa-hovercard'
-        
-        link.addEventListener("mouseover", (e) => {
-            let hovercard = e.target.querySelector(".sa-hovercard")
+  while (true) {
+    let link = await addon.tab.waitForElement("a[href*='/users/']", { markAsSeen: true });
+    let user = link.href.replace(/(.*)\/users\/((?:[a-z]|[A-Z]|[0-9]|-|_)+)\//g, "$2");
 
-            if (!hovercard.getAttribute('data-loaded')) {
-                hovercard.innerHTML = ''
-                hovercard.setAttribute('data-loaded', true)
+    const hovercard = document.createElement("div");
+    hovercard.className = "sa-hovercard";
 
-                const left = document.createElement('div')
-                left.className = 'sa-hovercard-left'
+    link.addEventListener("mouseover", (e) => {
+      let hovercard = e.target.querySelector(".sa-hovercard");
 
-                const pfp = document.createElement('img')
-                pfp.className = 'sa-hovercard-pfp'
-                left.appendChild(pfp)
+      if (!hovercard.getAttribute("data-loaded")) {
+        hovercard.innerHTML = "";
+        hovercard.setAttribute("data-loaded", true);
 
-                const username = document.createElement('span')
-                username.className = 'sa-hovercard-username'
-                username.innerText = user
-                left.appendChild(username)
+        const left = document.createElement("div");
+        left.className = "sa-hovercard-left";
 
-                fetch(`https://api.scratch.mit.edu/users/${user}`).then(res => res.json()).then(res => {
-                    pfp.src = res.profile.images['90x90']
-                    hovercard.appendChild(left)
-                })
-            }
-        })
+        const pfp = document.createElement("img");
+        pfp.className = "sa-hovercard-pfp";
+        left.appendChild(pfp);
 
-        hovercard.innerText = 'Loading'
-        link.appendChild(hovercard)
-    }
+        const username = document.createElement("span");
+        username.className = "sa-hovercard-username";
+        username.innerText = user;
+        left.appendChild(username);
+
+        fetch(`https://api.scratch.mit.edu/users/${user}`)
+          .then((res) => res.json())
+          .then((res) => {
+            pfp.src = res.profile.images["90x90"];
+            hovercard.appendChild(left);
+          });
+      }
+    });
+
+    hovercard.innerText = "Loading";
+    link.appendChild(hovercard);
+  }
 }
